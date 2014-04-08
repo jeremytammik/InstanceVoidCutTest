9765c33494daec639ef24565842af34bace6156c
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2014-04-08 15:42:58 +0200

    updated version number and description

--- InstanceVoidCutTest/Properties/AssemblyInfo.cs
+++ InstanceVoidCutTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "Revit Add-In Title" )]
-[assembly: AssemblyDescription( "Revit Add-In Description" )]
+[assembly: AssemblyTitle( "InstanceVoidCutTest Revit Add-In" )]
+[assembly: AssemblyDescription( "Test InstanceVoidCutUtils" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Autodesk Inc." )]
-[assembly: AssemblyProduct( "Revit Add-In Wizard Template Application" )]
-[assembly: AssemblyCopyright( "Copyright 2013 © Jeremy Tammik Autodesk Inc." )]
+[assembly: AssemblyProduct( "InstanceVoidCutTest Revit Add-In" )]
+[assembly: AssemblyCopyright( "Copyright 2014 © Jeremy Tammik Autodesk Inc." )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "2014.0.0.0" )]
+[assembly: AssemblyFileVersion( "2014.0.0.0" )]
